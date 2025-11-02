b3f29bb365886c7cd43cf8347d430b9d7d30479f
mos_skw <3235182831@qq.com>
2025-11-02 11:33:31 +0800

    PID增加

--- text-2/Users/main.c
+++ text-2/Users/main.c
@@ -6,6 +6,7 @@
 #include "Serial.h"
 #include "Encoder.h"
 #include <string.h>
+#include <stdio.h>
 #include "Timer.h"
 
 
@@ -13,7 +14,8 @@ uint8_t KeyNum;
 uint8_t bianji;
 char k;
 int16_t speed=0;
-double kp,ki,kd;
+float kp=0.1,ki=0,kd=0,target;
+float actual[2],out[2],err0[2],err1[2],errsum[2];
 int main(void)
 {
 	OLED_Init();
@@ -72,7 +74,7 @@ int main(void)
 				k=Serial_RxPacket[i];
 				speed=speed*10+(int)k;
 			}
-			//电脑输入，电机旋转
+			Motor1_SetSpeed(speed);//电脑输入，电机旋转
 		}
 		else if(bianji==1)
 		{
@@ -86,7 +88,43 @@ int main(void)
 
 
 
-void TIM2_IRQHandler(void)
+void TIM2_IRQHandler(void)    //PID
 {
-	
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+	{
+		target=speed;
+		actual[0]=Encoder_Get1();
+		actual[1]=Encoder_Get2();
+		for (int i=0;i<2;i++)
+		{
+			err1[i]=err0[i];
+			err0[i]=target-actual[i];
+			errsum[i]+=err0[i];
+			
+			out[i]=kp*err0[i]+ki*errsum[i]+kd*(err0[i]-err1[i]);
+			if (out[i]>100)
+			{
+				out[i]=100;
+			}
+			else if (out[i]<-100)
+			{
+				out[i]=-100;
+			}
+			if (actual[i]==0&&target==0)
+			{
+				out[i]=0;
+				errsum[i]=0;
+			}
+			if (i==0)
+			{
+				Motor1_SetSpeed(out[i]);
+			}
+			else if (i==1)
+			{
+				Motor2_SetSpeed(out[i]);
+			}
+		}
+		printf("%f,%f,%f,%f,%f\n",actual[0],actual[1],out[0],out[1],target);
+		TIM_ClearITPendingBit(TIM2 , TIM_IT_Update);
+	}
 }
